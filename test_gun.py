f7dc70413f08661d7f3bd4289fe8805466eeb96c
Samuel <samuel.rkz12@gmail.com>
2017-11-07 14:42:52 -0600

    Creating tests file

---
+++ test_gun.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestGun(TestCase):
+    def test_lock(self):
+        self.fail()
+
+    def test_unlock(self):
+        self.fail()
+
+    def test_isLock(self):
+        self.fail()
+
+    def test_shoot(self):
+        self.fail()
+
+    def test_reload(self):
+        self.fail()
