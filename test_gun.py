7a77f9aa40012d5dab1d53b76aee5bebe9e5df2d
Miguel Navarrete <miguel.angel.navarrete.meza@gmail.com>
2017-11-07 15:53:16 -0600

    Unlock and Lock Test Added

--- test_gun.py
+++ test_gun.py
@@ -1,5 +1,21 @@
 from unittest import TestCase
+from main import Gun
+import sys
 
+sys.tracebacklimit = 0
 
 class TestGun(TestCase):
-    pass
+    def setUp(self):
+        self.gun = Gun(5)
+
+    def test_lock(self):
+        """-- Test Gun Lock"""
+        msg = "The lock position in the gun is incorrect"
+        self.gun.lock()
+        self.assertTrue(self.gun.isLock, msg = msg)
+
+    def test_unlock(self):
+        """-- Test Gun Unlock"""
+        msg = "The lock position in the gun is incorrect"
+        self.gun.unlock()
+        self.assertFalse(self.gun.isLock, msg = msg)
