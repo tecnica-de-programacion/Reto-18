59207307f27b4dcba6d2bc91479fb8bfdd38b88d
Alejandro Mendoza <zippi180@gmail.com>
2017-11-07 17:36:50 -0600

    Updated test to verify ammunition load

--- test_gun.py
+++ test_gun.py
@@ -4,6 +4,15 @@ import sys
 sys.tracebacklimit = 0
 
 
-
 class TestGun(TestCase):
-    pass
+    def setUp(self):
+        print(self._testMethodDoc)
+
+    def test_load_gun(self):
+        """-- Test Load Gun"""
+        msg = "The ammunition is not being correctly loaded"
+        self.assertEqual(Gun(10).reload('hola'), 'hola', msg=msg)
+        self.assertEqual(Gun(10).reload(-4), None, msg=msg)
+        self.assertEqual(Gun(10).reload(10), 0, msg=msg)
+        self.assertEqual(Gun(10).reload(12), 2, msg=msg)
+
