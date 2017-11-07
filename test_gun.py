1f282540b9795dda590400efc7d0bf5ef729756d
David <deyivid@live.com>
2017-11-06 23:19:32 -0600

    first commit, create test file

---
+++ test_gun.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestGun(TestCase):
+    def test_lock(self):
+        pass
+
+    def test_unlock(self):
+        pass
+
+    def test_isLock(self):
+        pass
+
+    def test_shoot(self):
+        pass
+
+    def test_reload(self):
+        pass
