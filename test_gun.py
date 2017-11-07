457ac44eec68a42e46b5f35cb4e2240fbedccc2c
David <deyivid@live.com>
2017-11-06 23:32:48 -0600

    adding modules

--- test_gun.py
+++ test_gun.py
@@ -1,18 +1,26 @@
 from unittest import TestCase
+from main import Gun
+import sys
+sys.tracebacklimit = 0
 
 
 class TestGun(TestCase):
     def test_lock(self):
+        """-- Test Locked Gun"""
         pass
 
     def test_unlock(self):
+        """-- Test Unlocked Gun"""
         pass
 
     def test_isLock(self):
+        """-- Test If Gun is Lock"""
         pass
 
     def test_shoot(self):
+        """-- Test Shoot"""
         pass
 
     def test_reload(self):
+        """-- Test Reload """
         pass
