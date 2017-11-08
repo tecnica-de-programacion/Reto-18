e2686d81c2b51520729e8a99ffa2d319aef66c9c
Samuel <samuel.rkz12@gmail.com>
2017-11-07 22:58:44 -0600

    reload test

--- test_gun.py
+++ test_gun.py
@@ -28,10 +28,20 @@ class TestGun(TestCase):
         msg = "The correct type is not being returned for isLock"
         self.assertIsInstance(magnum.isLock, bool, msg=msg)
 
+    def test_reload(self):
+        """-- Test Reloading """
+        msg = "The gun is reloading wrong"
+        self.assertIsNone(magnum.reload(-5), msg=msg)
+        self.assertNotIsInstance(magnum.reload(4.5), int, msg=msg)
+        self.assertNotIsInstance(magnum.reload("assaf"), int, msg=msg)
+        self.assertEqual(magnum.reload(4.5), 4.5, msg=msg)
+        self.assertEqual(magnum.reload(12),2, msg=msg)
+        self.assertEqual(magnum.reload(12), 12, msg=msg)
+
     def test_shoot(self):
         """-- Test shoot gun"""
         msg = "The gun is not shooting"
         msg_2 = "The gun is shooting locked"
         self.assertTrue(magnum.shoot, msg=msg)
         magnum.lock()
-        self.assertTrue(magnum.shoot, msg = msg_2)
+        self.assertTrue(magnum.shoot, msg=msg_2)
