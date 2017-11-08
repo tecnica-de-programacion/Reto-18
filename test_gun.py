4c00f8f9fdfae191c2a49b733b26593977e01c61
RicardoSaid <said_s027@hotmail.es>
2017-11-07 21:46:53 -0600

    I added the lock und unlock test

--- test_gun.py
+++ test_gun.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from mainte import Gun
 import sys
 sys.tracebacklimit = 0
+one_gun = Gun(10)
 
 class Test_Gun(TestCase):
     def setUp(self):
@@ -10,23 +11,27 @@ class Test_Gun(TestCase):
     def tearDown(self):
         pass
 
+    def test_lock_gun(self):
+        """-- Test lokc gun"""
+        msg = "The gun is unlock"
+        one_gun.lock()
+        self.assertTrue(one_gun.isLock, msg = msg)
+
     def test_the_lock_of_gun(self):
         """-- Test the lock of the gun"""
         msg = "The safe of gun is unlock"
-        self.assertFalse(Gun.lock(self), msg = msg)
+        one_gun.unlock()
+        self.assertFalse(one_gun.isLock, msg = msg)
+
 
     def test_the_unlock_of_gun(self):
         """-- Test the unlock of the gun"""
-        msg= "The safe of gunis lock"
-        self.assertFalse(Gun.unlock(self), msg = msg)
-
-    def test_ammount_of_cartridge(self):
-        """-- Test ammount of cartridge in the gun"""
-        pass
+        msg= "The safe of gun is lock"
 
     def test_shoot_gun(self):
         """-- Test shoot gun"""
-        pass
+        msg = "When the gun shoot the safe must lock"
+
 
     def test_no_cartridge(self):
         """-- Test no cartridge in the gun"""
