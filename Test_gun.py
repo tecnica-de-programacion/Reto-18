727dc8f3f91a4cc61e7b263db5ba8ece2b0be33b
victor hugo vasquez <victorh_vn@hotmail.com>
2017-11-07 22:14:51 -0600

    all test was added

--- Test_gun.py
+++ Test_gun.py
@@ -14,27 +14,36 @@ class Test_my_Gun(TestCase):
         msg = "Error with function isLock"
         self.assertIsNotNone(self.my_gun.isLock, msg=msg)
 
-    def test_unLock_gun(self):
-        """Test, unlock my gun"""
-        msg = "My gun is lock yet"
-        self.my_gun.unlock()
-        self.assertIs(self.my_gun.isLock, False, msg=msg)
+
 
     def test_lock_gun(self):
         """Test, lock my gun"""
         msg = "My gun has not locked"
         self.my_gun.lock()
         self.assertIs(self.my_gun.isLock, True, msg=msg)
-   
+
+    def test_unLock_gun(self):
+        """Test, unlock my gun"""
+        msg = "My gun is lock yet"
+        self.my_gun.unlock()
+        self.assertIs(self.my_gun.isLock, False, msg=msg)
+
 
     def test_shooting_gun(self):
         """Test, shooting my gun"""
-        pass
+        msg = "Fail shoot"
+        self.my_gun.lock()
+        self.assertIsNone(self.my_gun.shoot(), msg=msg)
+
+
 
 
     def test_reload_gun(self):
         """Test, reload my gun"""
-        pass
+        msg = "Fail reload"
+        self.assertNotIsInstance(self.my_gun.reload(23.09), int, msg=msg)
+        self.assertIsNone(self.my_gun.reload(-99), msg=msg)
+        self.assertIsInstance(self.my_gun.reload(10), int, msg=msg)
 
 
 
