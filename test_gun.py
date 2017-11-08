1c9a4c87d4dca999ab56a5dbd2a626fca527a7ac
Samuel <samuel.rkz12@gmail.com>
2017-11-07 21:01:21 -0600

    Test for reload and shoot

--- test_gun.py
+++ test_gun.py
@@ -3,6 +3,7 @@ from main import Gun
 import sys
 sys.tracebacklimit = 0
 magnum = Gun(10)
+
 class TestGun(TestCase):
     def setUp(self):
         print(self._testMethodDoc)
@@ -23,12 +24,14 @@ class TestGun(TestCase):
         self.assertFalse(magnum.isLock, msg=msg)
 
     def test_isLock(self):
-        """"-- Test Int isLock"""
+        """-- Test Int isLock"""
         msg = "The correct type is not being returned for isLock"
         self.assertIsInstance(magnum.isLock, bool, msg=msg)
 
-    #def test_shoot(self):
-        #self.fail()
-
-    #def test_reload(self):
-        #self.fail()
+    def test_shoot(self):
+        """-- Test shoot gun"""
+        msg = "The gun is not shooting"
+        msg_2 = "The gun is shooting locked"
+        self.assertTrue(magnum.shoot, msg=msg)
+        magnum.lock()
+        self.assertTrue(magnum.shoot, msg = msg_2)
