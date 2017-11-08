e36cd06d7f8ec109f1cad7cc30ca0d2e05fe9022
OhtokaniHernandezAQ <antoniohernandezq.177@gmail.com>
2017-11-07 22:57:41 -0600

    terminando los testings

--- test_gun.py
+++ test_gun.py
@@ -1,5 +1,47 @@
 from unittest import TestCase
-
+from main import Gun
+import sys
+sys.tracebacklimit = 0
 
 class TestGun(TestCase):
-    pass
+    def setUp(self):
+        self.gun = Gun(7)
+        print(self._testMethodDoc)
+
+    def tearDown(self):
+        pass
+
+    def test_lock(self):
+        """-- Test Lock"""
+        msg = "The lock posicion is wrong"
+        gun = Gun(7)
+        self.gun.lock()
+        self.assertTrue(self.gun.isLock, msg=msg)
+
+    def test_unlock(self):
+        """-- Test Unlock"""
+        msg = "The lock posicion is wrong"
+        gun = Gun(7)
+        self.gun.unlock()
+        self.assertFalse(self.gun.isLock, msg=msg)
+
+    def test_shoot(self):
+        """-- Test Shoot"""
+        msg = "The gun can not shoot"
+        gun = Gun(7)
+        self.gun.reload(4)
+        self.assertEqual(self.gun.shoot(), None, msg=msg)
+        self.gun.lock()
+        self.assertEqual(self.gun.shoot(), None, msg=msg)
+        self.gun.unlock()
+        self.assertEqual(self.gun.shoot(), None, msg=msg)
+
+
+    def test_reload(self):
+        """-- Test Reload"""
+        msg = "The gun can not shoot"
+        gun = Gun(7)
+        self.assertEqual(self.gun.reload(12), 5, msg=msg)
+        self.assertEqual(self.gun.reload(4.8), 4.8, msg=msg)
+        self.assertEqual(self.gun.reload(-15), None, msg=msg)
+
